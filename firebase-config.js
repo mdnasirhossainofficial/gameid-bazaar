e478053f35573027d2cdb832b38c6793e7db0cf4
mdnasirhossainofficial <mdnasirhossainofficialbd@gmail.com>
2025-07-26 14:25:17 +0600

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkHH8U_zgWQ01SLLhqrnxhe8j6KjnM9Q0",
+  authDomain: "bismillah-telecom-ed960.firebaseapp.com",
+  databaseURL: "https://bismillah-telecom-ed960-default-rtdb.firebaseio.com",
+  projectId: "bismillah-telecom-ed960",
+  storageBucket: "bismillah-telecom-ed960.appspot.com",
+  messagingSenderId: "163310183508",
+  appId: "1:163310183508:web:036a2421c0cd3c264079c1",
+  measurementId: "G-J86C7BKL10"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
